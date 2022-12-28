73a9063f17e7cf3511d9f84650c4db74ce63a136
Joao Paulo <oliveirajpufrj@gmail.com>
2022-12-28 14:11:14 -0300

    feat: fallback showing available routes when try to 'get' a non-existent routes

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,13 @@ Route::group(['prefix' => 'v1'], function () {
     Route::post('/pessoa', 'Api\PessoaController@store');
     Route::put('/pessoa/{id}', 'Api\PessoaController@update');
     Route::delete('/pessoa/{id}', 'Api\PessoaController@destroy');
+
+    Route::fallback(function () {
+        return response()->json([
+            'sucess' => false,
+            'payload' => 'Route Not Found.',
+            'message'=> "You can check an array with routes names on 'routes' key",
+            'routes'=>['get/pessoa','get/pessoa{id}','post/pessoa','put/pessoa{id}','delete/pessoa/{id}']
+        ], 404);
+    });
 });
